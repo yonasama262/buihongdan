d4f5851dc9125598d8be04627187978cbd570b11
yonasama262 <dannamvietkg@gmail.com>
2025-10-15 18:04:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,9 @@ def run_gradcam(image: Image.Image):
     try:
         transform = transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.ToTensor()
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
         ])
         input_tensor = transform(image).unsqueeze(0)
 
